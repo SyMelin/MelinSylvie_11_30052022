096c5f7159d9d0c5e0afb8b2d72817359a045bba
Sylvie Melin <sy.melin@gmail.com>
2022-06-02 20:12:53 +0200

    clickable Dropdown

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,16 +1,37 @@
-import '../../styles/Dropdown.css'
+import { useState, useEffect } from 'react'
 import arrowUp from '../../assets/arrowUp.svg'
+import arrowDown from '../../assets/arrowDown.svg'
+import '../../styles/Dropdown.css'
 
 function Dropdown ({title, text}) {
-    return (
+    const [isOpen, setIsOpen] = useState(false)
+
+    return isOpen ? (
         <div className="dropdown__container">
             <div className='dropdown__top'>
                 <h2 className="dropdown__title">{title}</h2>
-                <img className="dropdown__button" src={arrowUp} alt="Fermer" />
+                <img
+                    className="dropdown__button"
+                    src={arrowUp}
+                    alt="Fermer"
+                    onClick={() => setIsOpen(false)}
+                />
             </div>
              { typeof text === 'object' ?
                 <ul className="dropdown__bottom dropdownList">{text.map((item) => <li>{item}</li>)}</ul>
                 : <p className="dropdown__bottom dropdownText">{text}</p>}
+        </div>
+    ) : (
+        <div className="dropdown__container">
+            <div className='dropdown__top'>
+                <h2 className="dropdown__title">{title}</h2>
+                <img
+                    className="dropdown__button"
+                    src={arrowDown}
+                    alt="Fermer"
+                    onClick={() => setIsOpen(true)}
+                />
+            </div>
         </div>
     )
 }
