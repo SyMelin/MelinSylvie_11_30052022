7af7fb585fd5821315ee99da232fd4fd4cc3c628
Sylvie Melin <sy.melin@gmail.com>
2022-06-08 19:37:21 +0200

    nav__link--underlined

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,20 @@
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom';
 import logo from '../../assets/logo.svg'
 import '../../styles/Header.css'
 
 function Header() {
+  const location = useLocation();
+  const pathname = location.pathname
+
     return (
         <header className='header'>
           <Link to="/" className='logo'>
               <img src={logo} alt="Kasa" className='logo--img'/>
           </Link>
           <nav className='nav'>
-            <Link to="/" className='nav__link'>Accueil</Link>
-            <Link to="/about" className='nav__link'>A propos</Link>
+            <Link to="/" className={ pathname === '/' ? 'nav__link nav__link--underlined' : 'nav__link nav__link--notUnderlined'}>Accueil</Link>
+            <Link to="/about" className={ pathname === '/about' ? 'nav__link nav__link--underlined' : 'nav__link nav__link--notUnderlined'}>A propos</Link>
           </nav>
         </header>    
     )
