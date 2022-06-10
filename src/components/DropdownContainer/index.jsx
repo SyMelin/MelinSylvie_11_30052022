641a62664b5379ea248a62bc612ee1713e2667a0
Sylvie Melin <sy.melin@gmail.com>
2022-06-10 14:49:43 +0200

    dropdownContainer comments

--- src/components/DropdownContainer/index.jsx
+++ src/components/DropdownContainer/index.jsx
@@ -8,9 +8,10 @@ function DropdownContainer({ dropdownTitles, dropdownTexts, type}) {
     //On crée les dropdowns de la page
     if (dropdownTitles.length === dropdownTexts.length) {
         //A chaque titre, on crée un objet de type ' const dropdownContent= {title:"", text:""}  ' qui regroupe le titre dropdown et son texte
+        //Tous les objects dropdownContent sont récupérés dans le tableau dropdownList
         dropdownsList = dropdownTitles.map((title, index) => { return { title: title, text: dropdownTexts[index] } })
     } else {
-        console.log('error: lenghts of dropdownTitles and dropdownText should be equal')
+        console.log('error: lengths of dropdownTitles and dropdownText should be equal')
     }
     
     return (
