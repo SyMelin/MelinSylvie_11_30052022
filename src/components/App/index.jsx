8b3d937bded44387bcc4000b3d9edef9d3044513
Sylvie Melin <sy.melin@gmail.com>
2022-06-14 09:19:27 +0200

    creation component App

---
+++ src/components/App/index.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../../pages/Home';
+import Accomodation from '../../pages/Accomodation';
+import About from '../../pages/About/index'
+import Header from '../Header';
+import Footer from '../Footer';
+import Error from '../Error';
+
+function App() {
+    return [
+        <Router>
+            <Header />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/accomodation/:accomodationId" element={<Accomodation />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+            <Footer />
+        </Router>
+    ]
+}
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/index';
-import Accomodation from './pages/Accomodation';
-import About from './pages/About/index'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Error from './components/Error';
+import App from './components/App';
 import './styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/accomodation/:accomodationId" element={<Accomodation />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <App />
   </React.StrictMode>
 );
