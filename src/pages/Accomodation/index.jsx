9fc73e26b2ccf97239df3350095ee298926cbd45
Sylvie Melin <sy.melin@gmail.com>
2022-06-27 11:21:09 +0200

    correction suite à soutenance

--- src/pages/Accomodation/index.jsx
+++ src/pages/Accomodation/index.jsx
@@ -2,6 +2,7 @@ import { useParams, useNavigate } from 'react-router-dom'
 import Gallery from '../../components/Gallery'
 import Stars  from '../../components/Stars'
 import DropdownsWrapper from '../../components/DropdownsWrapper'
+import Error from '../../components/Error'
 import { useFetch } from '../../utils/hooks'
 import '../../styles/Accomodation.css'
 
@@ -21,54 +22,56 @@ function Accomodation() {
         const accomodationsList = [...data]
        // console.log('acc', accomodationsList)
         const accomodationData = accomodationsList.find(accomodation => accomodation.id === accomodationId)
-       // console.log(accomodationData)
 
-        //On sépare le prénom du nom pour pouvoir afficher l'ensemble sur 2 lignes
-        const hostName = accomodationData.host.name.split(" ")
+        if (accomodationData) {
+             //On sépare le prénom du nom pour pouvoir afficher l'ensemble sur 2 lignes
+            const hostName = accomodationData.host.name.split(" ")
 
-        //Les dropdowns sont composés de 2 sous-parties: un titre et un contenu
-        //On renseigne alors les tableaux: dropdownTitles qui contient les titres, dropdownTexts qui contient le texte
-        const dropdownTitles = ["Description", "Équipements"]
-        const dropdownTexts= [accomodationData.description, accomodationData.equipments] //Nb: description est une chaîne de caractère, equipments est un tableau
+            //Les dropdowns sont composés de 2 sous-parties: un titre et un contenu
+            //On renseigne alors les tableaux: dropdownTitles qui contient les titres, dropdownTexts qui contient le texte
+            const dropdownTitles = ["Description", "Équipements"]
+            const dropdownTexts= [accomodationData.description, accomodationData.equipments] //Nb: description est une chaîne de caractère, equipments est un tableau
 
-        return (
-            <div className='accomodation__container'>
-            <Gallery
-                pics={accomodationData.pictures}
-                length={accomodationData.pictures.length}
-                accId={accomodationId}
-            />
-            <div className='accomodation__info'>
-                <div className='info__left'>
-                    <div className='accomodation__data'>
-                        <h1 className="accomodation__title">{accomodationData.title}</h1>
-                        <p className='accomodation__location'>{accomodationData.location}</p>
-                        <div className='accomodation__tags'>
-                            {accomodationData.tags.map((tag, index) => (
-                                <div key={`tag--${index}`} className='tag'>{tag}</div>
-                            ))}
+            return (
+                <div className='accomodation__container'>
+                    <Gallery
+                        pics={accomodationData.pictures}
+                        length={accomodationData.pictures.length}
+                        accId={accomodationId}
+                    />
+                    <div className='accomodation__info'>
+                        <div className='info__left'>
+                            <div className='accomodation__data'>
+                                <h1 className="accomodation__title">{accomodationData.title}</h1>
+                                <p className='accomodation__location'>{accomodationData.location}</p>
+                                <div className='accomodation__tags'>
+                                    {accomodationData.tags.map((tag, index) => (
+                                        <div key={`tag--${index}`} className='tag'>{tag}</div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div className='info__right'>
+                            <div className='host__data'>
+                                <p className='host__name'>{hostName[0]}<br />{hostName[1]}</p>
+                                <img className='host__picture' src={accomodationData.host.picture} alt="" />
+                            </div>
+                            <Stars
+                                rating={accomodationData.rating}
+                                arrayLength='5'
+                            />
                         </div>
                     </div>
-                </div>
-                <div className='info__right'>
-                    <div className='host__data'>
-                        <p className='host__name'>{hostName[0]}<br />{hostName[1]}</p>
-                        <img className='host__picture' src={accomodationData.host.picture} alt="" />
-                    </div>
-                    <Stars
-                        rating={accomodationData.rating}
-                        arrayLength='5'
+                    <DropdownsWrapper
+                        dropdownTitles={dropdownTitles}
+                        dropdownTexts={dropdownTexts}
+                        type='accomodation'
                     />
-                </div>
-            </div>
-            <DropdownsWrapper
-                dropdownTitles={dropdownTitles}
-                dropdownTexts={dropdownTexts}
-                type='accomodation'
-            />
-        </div>
-            
-        )
+                </div>  
+            )
+        }else {
+            return <Error />
+        }
     }
 }
 
